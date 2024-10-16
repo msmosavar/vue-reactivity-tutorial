0e1159ea06df4c10b48f8c90019d05733de3ce02
msmosavar <sadeq.develop@gmail.com>
2024-10-16 11:05:11 +0330

    vue2 reactivity

--- main.js
+++ main.js
@@ -1,72 +1,109 @@
-let a = 1;
-let b = 2;
-const sum = a + b;
+// Do you know about object getters and setters
 
-console.log(sum) // 3
-
-a = 2;
-
-console.log(sum) // ?
-
-// how we can change sum when a or b changes?
-
-// maybe some magic operand like $...
-
-let d = 10
-let e = 20
-// const sum1 $= d + e 
-
-console.log(sum1) // 30
-
-d = 15
-console.log(sum1) // 35
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ------------ Solution 1: Functions -----------
-let f = 5
-let g = 7
-
-function calcSum() {
-  return f + g
+const obj = {
+  value: 1
 }
 
-let sum3 = calcSum()
+console.log(obj.value) // 1
 
-console.log(sum3) // 12
+obj.value = 2
 
-// how to update sum when f changes?
-f = 10
-console.log(sum3) // 12  ... Oops!
+console.log(obj.value) // 2
 
-sum3 = calcSum()
-console.log(sum3) // 15  ... Yesk!
+function alaki() {
+  console.log('alaki is called')
+}
 
-// but there is no magic here
+const obj2 = {
+  _value: 1,
+  get value() {
+    console.log('getter is called')
+    return this._value
+  },
+  set value(newValue) {
+    console.log('setter is called')
+    this._value = newValue
+  }
+}
+
+obj2.value
+obj2.value = 3
+
+
+
+
+
+
+
+// Vue2
+const count = ref(0);
+// Log the count when it changes
+effect(() => {
+  console.log(`Count: ${count.value}`);
+});
+// Simulate button clicks
+function increment() {
+  count.value++;
+}
+function decrement() {
+  count.value--;
+}
+function reset() {
+  count.value = 0;
+}
+// Example usage
+increment(); // Logs: Count: 1
+increment(); // Logs: Count: 2
+decrement(); // Logs: Count: 1
+reset(); // Logs: Count: 0
+
+function ref(initialValue) {
+  const r = {
+    get value() {
+      track(r, 'value');
+      return initialValue;
+    },
+    set value(newValue) {
+      initialValue = newValue;
+      trigger(r, 'value');
+    },
+  };
+  return r;
+}
+
+let activeEffect;
+const targetMap = new Map();
+function track(target, key) {
+  if (activeEffect) {
+    let depsMap = targetMap.get(target);
+    if (!depsMap) {
+      targetMap.set(target, (depsMap = new Map()));
+    }
+    let dep = depsMap.get(key);
+    if (!dep) {
+      depsMap.set(key, (dep = new Set()));
+    }
+    dep.add(activeEffect);
+  }
+}
+function trigger(target, key) {
+  const depsMap = targetMap.get(target);
+  if (!depsMap) return;
+  const dep = depsMap.get(key);
+  if (dep) {
+    dep.forEach((effect) => effect());
+  }
+
+
+  function effect(fn) {
+    const effect = () => {
+      activeEffect = effect;
+      try {
+        fn();
+      } finally {
+        activeEffect = null;
+      }
+    };
+    effect();
+    return effect;
+  }
